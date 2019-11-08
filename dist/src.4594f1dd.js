0f5f2da1d847173957c3305308a10c5d18be0f9f
Gregory Romantsov <imoonserk@gmail.com>
2019-11-08 21:13:04 +0400

    init commit

---
+++ dist/src.4594f1dd.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"zbzq":[function(require,module,exports) {
+
+},{}],"aIbH":[function(require,module,exports) {
+
+},{"./../assets/logo.png":[["logo.7f05b51f.png","p7Gf"],"p7Gf"]}],"mxek":[function(require,module,exports) {
+
+},{"./../assets/title.png":[["title.91b3f5e3.png","davG"],"davG"]}],"T1g5":[function(require,module,exports) {
+
+},{"./../assets/clock-circular-outline.png":[["clock-circular-outline.dfcea929.png","CPSH"],"CPSH"]}],"Focm":[function(require,module,exports) {
+require("./style.scss"),require("./header/header.scss"),require("./title/title.scss"),require("./cards/card.scss"),require("./cards/card_layout.scss"),require("./cards/left_menu.scss");
+},{"./style.scss":"zbzq","./header/header.scss":"aIbH","./title/title.scss":"mxek","./cards/card.scss":"T1g5","./cards/card_layout.scss":"zbzq","./cards/left_menu.scss":"zbzq"}]},{},["Focm"], null)
+//# sourceMappingURL=/src.4594f1dd.js.map
---
+++ src/index.js
@@ -0,0 +1,6 @@
+require('./style.scss');
+require('./header/header.scss');
+require('./title/title.scss');
+require('./cards/card.scss');
+require('./cards/card_layout.scss');
+require('./cards/left_menu.scss');
